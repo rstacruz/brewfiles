02b54836e20c9d20e7a0f16f6ad3c0b4d6cb7d62
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-11-01 01:59:54 +0800

    Update

--- rstacruz-air15/misc.sh
+++ rstacruz-air15/misc.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 
-# iojs 2.5.0
-brew install https://raw.githubusercontent.com/aredridel/homebrew-iojs/0c8a4741dfbfaeec1edcadca8511555a5149f475/Formula/iojs.rb
-
 # envy code r
 brew cask install https://raw.githubusercontent.com/caskroom/homebrew-fonts/02b4f2e47e82dfda3e83a5c6b3145cbf5ff5c9ad/Casks/font-envy-code-r.rb
